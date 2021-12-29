c08288e6293f71e0e5551e7a5fb9aaec340a5d6e
Efremov Danil <naturforscher@outlook.com>
2021-12-29 22:02:22 +0300

    Some additionals

---
+++ Assets/_Scripts/View/LevelObjectView.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace PlatformerMVC.View
+{
+    public class LevelObjectView : MonoBehaviour
+    {
+
+       public Transform _Transform;
+       public SpriteRenderer SpriteRenderer;
+       public Rigidbody2D _rigidbody2D;
+       public Collider2D _collider2D;
+    }
+}
